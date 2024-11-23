4c31585b30407b7afc6495554dd378dd00b7829e
Recxo <rishava065@gmail.com>
2024-11-23 13:06:31 +0530

    added some new files

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -8,5 +8,20 @@ from sklearn.model_selection import train_test_split
 from typing import Union 
 
 class DataStrategy(ABC):
+    
+    @abstractmethod
+    def handle_data(self,data):
+        pass 
+
+
+class DataPreProcessingStrategy(DataStrategy):
     pass
+
+
+class DataDivideStrategy(DataStrategy):
+    pass
+
+class DataCleaning(DataStrategy):
+    pass
+
 
