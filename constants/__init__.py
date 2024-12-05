91a2038bd3839942614ee2a7db8814e914d23072
Recxo <rishava065@gmail.com>
2024-12-05 12:17:29 +0530

    removing 'state' column  because it does not has any use

--- constants/__init__.py
+++ constants/__init__.py
@@ -1,2 +1,2 @@
 TARGET_COLUMN = "churn"
-COLS_TO_DROP = [' "recordID"','customer_id','total_day_charge','total_eve_charge','total_night_charge','total_intl_charge']
+COLS_TO_DROP = [' "recordID"','state','customer_id','total_day_charge','total_eve_charge','total_night_charge','total_intl_charge']
