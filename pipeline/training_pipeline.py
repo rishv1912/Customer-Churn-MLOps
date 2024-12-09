452dc902bb26c7848bfaa1b8f877e5a344abd377
Recxo <rishava065@gmail.com>
2024-12-09 13:11:59 +0530

    Running of the project has been done using decision tree now handling the imbalannce of data

--- pipeline/training_pipeline.py
+++ pipeline/training_pipeline.py
@@ -9,4 +9,5 @@ def train_pipeline(data_path):
     X_train,X_test,y_train,y_test = clean_df(df)
     model = train_model(X_train,X_test,y_train,y_test)
     precision,recall,f1_scr,roc_auc = evaluate_model(model,X_test,y_test)
+    # this print is temporary to check is it working well or not, giving any error
     print(f"Precision {precision}\nRecall {recall}\nF1 Score {f1_scr}\nRoc_AUC {roc_auc}")
