55e6364e238f242c4e91c42fc8e74ae4dd04eede
Recxo <rishava065@gmail.com>
2025-03-15 21:36:03 +0530

    run the model after months

--- pipeline/training_pipeline.py
+++ pipeline/training_pipeline.py
@@ -14,12 +14,12 @@ def train_pipeline(data_path):
     model = train_model(X_train,X_test,y_train,y_test)
     precision,recall,f1_scr,roc_auc = evaluate_model(model,X_test,y_test)
     # this print is temporary to check is it working well or not, giving any error
-    # print("X_train",X_train.shape)
-    # print("X_test",X_test.shape)
-    # print("y_train",y_train.shape)
-    # print("y_test",y_test.shape)
-    # print(f"Precision {precision}\nRecall {recall}\nF1 Score {f1_scr}\nROC AUC {roc_auc}\nX_train len {len(X_train)}\nX_test {len(X_test)}")
-    # print(X_train.T.head(1))
+    print("X_train",X_train.shape)
+    print("X_test",X_test.shape)
+    print("y_train",y_train.shape)
+    print("y_test",y_test.shape)
+    print(f"Precision {precision}\nRecall {recall}\nF1 Score {f1_scr}\nROC AUC {roc_auc}\nX_train len {len(X_train)}\nX_test {len(X_test)}")
+    print(X_train.T.head(1))
 
 
 
