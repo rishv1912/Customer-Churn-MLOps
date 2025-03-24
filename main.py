564063d442e02f9753d68d2d76f92c4530883668
Recxo <rishava065@gmail.com>
2025-03-24 17:12:53 +0530

    :wq

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# @app.get("/")
+# def read_root():
+#     return {"message" : "Welcome to Customer Churn Prediction"}
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id : int , q : str = None):
+#     return {"item_id": item_id, "q":q}
 @app.get("/")
-
-
 def read_root():
-    return {"message" : "Welcome to Customer Churn Prediction"}
+    return {"message":"Hello World"}
 
 @app.get("/items/{item_id}")
-def read_item(item_id : int , q : str = None):
-    return {"item_id": item_id, "q":q}
+def read_item(item_id : int, query_param: str = None):
+    return{"item_id" : item_id, "query_param" : query_param}
