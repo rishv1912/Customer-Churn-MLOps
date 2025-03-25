8ab9c5eeb8c25d29ba262e754520fd9d7d8f3fb6
Recxo <rishava065@gmail.com>
2025-03-25 22:14:10 +0530

    will continue the fastapi at the end

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
-from fastapi import FastAPI
+# from fastapi import FastAPI, HTTPException
+# from enum import Enum
+# from pydantic import BaseModel
 
-app = FastAPI()
+# app = FastAPI()
+
+# class Category(Enum):
+#     TOOLS = 'tools'
+#     CONSUMABLES = 'consumables'
+
+# class Item(BaseModel):
+#     name: str
+#     price: float
+#     count: int
+#     id: int
+#     category: Category
+
+# items ={
+#     0 : Item(name="Kratos", price=8.99, count=20,id=0, category=Category.TOOLS),
+#     1 : Item(name="God", price=7.99, count=18,id=1, category=Category.TOOLS),
+#     2 : Item(name="Candy", price=4.99, count=10,id=2, category=Category.CONSUMABLES),
+# }
 
 # @app.get("/")
-# def read_root():
-#     return {"message" : "Welcome to Customer Churn Prediction"}
+# def read_root() -> dict[str,dict[int,Item]]:
+#     return {"items": items}
 
 # @app.get("/items/{item_id}")
-# def read_item(item_id : int , q : str = None):
-#     return {"item_id": item_id, "q":q}
-@app.get("/")
-def read_root():
-    return {"message":"Hello World"}
+# def query_item_by_id(item_id : int) -> Item:
+#     if item_id not in items:
+#         raise HTTPException(status_code=404, detail=f"Item with {item_id=} does not exist.")
+    
+#     return items(item_id)
 
-@app.get("/items/{item_id}")
-def read_item(item_id : int, query_param: str = None):
-    return{"item_id" : item_id, "query_param" : query_param}
+
+    
