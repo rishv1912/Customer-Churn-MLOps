5797f751a0f11cb3669a667a1f6e938c8ce96818
Recxo <rishava065@gmail.com>
2025-08-28 18:51:57 +0530

    adding ml pipeline for orchestration

--- airflow/dags/training.py
+++ airflow/dags/training.py
@@ -1,24 +1,21 @@
 from datetime import datetime
 from airflow import DAG
 from airflow.providers.standard.operators.bash  import BashOperator
+from airflow.providers.standard.operators.python  import PythonOperator
 from airflow.decorators import task
 
 with DAG(
-    dag_id="training",
+    dag_id="ML Pipeline",
     start_date=datetime(2025, 8, 24),
     schedule="0 0 * * *",
     catchup=False,
     tags=["training", "example1"]
 ) as dag:
     
-    hello = BashOperator(
-        task_id="train",
-        bash_command='echo hello'
-    )
+    def ml_pipeline():
 
-    @task
-    def airflow_task():
-        print("hello world from airflow train")
-        return "Task completed successfully"
-
-    hello >> airflow_task()
+        data_task = PythonOperator(
+            
+        )
+            
+    
